46c032ff063e720d55a19ea1e49548a8017cd574
ZoulfaALBawab <36267114+ZoulfaALBawab@users.noreply.github.com>
2018-02-20 09:49:17 +0200

    update Oop
    here is my code

--- Oop.js
+++ Oop.js
@@ -28,3 +28,34 @@ function makeGame(upperbound){
   	}
   }
 }
+
+function makeGame (upperbound) { 
+   counter = initial ;
+   makeGame.upper = upper ; 
+   makeGame.rand = rand;
+
+}
+ var guessMyNumber = function (n){
+  counter ++ 
+  var upper=upperbound;
+  var rand = randInt(upper);
+  if (n> this.upper){
+    return "Out of bounds! Please try a number between 0 and " + this.upperbound + ".";
+    } else if (n === this.rand) {
+        return "You guessed my number!";
+      }
+      return "Nope! That wasn't it!";
+ };
+
+ var giveUp = function (){
+    return this.rand ; 
+ };
+
+ var numOfGuesses = function (){
+  return this.counter;
+ };
+ ///////////////////////////////////////////////////////
+
+
+
+
